c40e45903fa7c413ea48b920f5a512e2da88ea17
Guilherme Ismael <guiismael@gmail.com>
2023-02-11 00:11:08 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center bg-gray-900">
+      <h1 className="text-8xl text-white font-bold">Coming soon</h1>
+    </div>
+  )
+}
